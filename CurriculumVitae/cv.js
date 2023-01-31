e734238e989c5ad43ac19a190291695b1755370b
Abelesq <124078048+Abelesq@users.noreply.github.com>
2023-01-31 14:15:40 -0300

    Add files via upload

---
+++ CurriculumVitae/cv.js
@@ -0,0 +1,5 @@
+
+	document.getElementById('boton').onclick = function (){
+	document.getElementById('demo').innerHTML = "Soy honesto, responsable y amable. Cumplo con mi trabajo, me adapto a cualquier rubro con facilidad."
+	document.getElementById('demo').style.fontFamily = 'Roboto Slab, serif'
+}
